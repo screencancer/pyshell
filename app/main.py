7b0bd2a9355fab39a5648326e5cdda8fe1009fad
MJohnson <48001236+screencancer@users.noreply.github.com>
2025-02-20 15:06:25 -0500

    First stage

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 import sys
-
+d = {
+    "exit 0": "valid" 
+}
 
 def main():
-
+    exit = False
     # Wait for user input
     while True:
         sys.stdout.write("$ ")
         command = input()
-        print(f"{command}: command not found")
-
+        if command not in d:
+            print(f"{command}: command not found")
+        if command == "exit 0":
+            print("test")
 
 if __name__ == "__main__":
     main()
