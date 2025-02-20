5497ad5e0c07a66a5acd333b84f8e6110c2ab310
MJohnson <48001236+screencancer@users.noreply.github.com>
2025-02-20 15:26:30 -0500

    Syntax checks

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def checkValidCommand(command):
     #print(command[0], d.values())
     if command[0] in d.keys():
         return True, command[0], command[1]
-    else:None
+    else:
         return False, command, None
 
 def main():
