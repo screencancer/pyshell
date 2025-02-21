71035ec422912a969dc12b17309fe4c6e34372c7
MJohnson <48001236+screencancer@users.noreply.github.com>
2025-02-21 10:44:35 -0500

    Syntax checks

--- app/main.py
+++ app/main.py
@@ -2,37 +2,40 @@ import os
 import sys
 
 d = {
-    "exit": "builtin" ,
+    "exit": "builtin",
     "echo": "builtin",
     "type": "builtin"
 }
 
+
 def checkPath(path):
     directories = str(path).split(":")
-    #print(directories, "all directories")
+    print(directories, "all directories")
     for dirs in directories:
         if os.path.exists(dirs):
             listTest = os.listdir(dirs)
-        #print(dirs, listTest)
+        # print(dirs, listTest)
         for file in listTest:
-            #print(file, "file is in ", dirs)
+            # print(file, "file is in ", dirs)
             if file in d.keys():
                 return dirs
     return "invalid"
+
+
 def checkValidCommand(command):
     command = str(command).split(" ", 1)
-    #print(command)
-    #print(command[0], d.values())
+    # print(command)
+    # print(command[0], d.values())
     if command[0] in d.keys():
         return True, command[0], command[1]
     else:
         return False, command[0], None
 
+
 def main():
     path = os.environ['PATH']
-    #print("Path is " , path)
     exit = False
-    # Wait for user input
+
     while True:
         sys.stdout.write("$ ")
         command = input()
@@ -50,7 +53,9 @@ def main():
                     print(f"{arg} is a shell {d.get(arg)}")
                 elif not newPath == "invalid":
                     print(f"{arg} is {newPath}/{arg}")
-                else: 
+                else:
                     print(f"{arg}: not found")
+
+
 if __name__ == "__main__":
     main()
